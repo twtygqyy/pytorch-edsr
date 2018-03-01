33c5a54a5acf5978fe69c349613b11c75864e59f
twtygqyy <twtygqyy@gmail.com>
2018-03-01 07:30:00 -0800

    update

--- edsr.py
+++ edsr.py
@@ -2,14 +2,24 @@ import torch
 import torch.nn as nn
 import math
 
+class MeanShift(nn.Conv2d):
+    def __init__(self, rgb_mean, sign):
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1)
+        self.bias.data = float(sign) * torch.Tensor(rgb_mean)
+
+        # Freeze the MeanShift layer
+        for params in self.parameters():
+            params.requires_grad = False
+
 class _Residual_Block(nn.Module): 
     def __init__(self):
         super(_Residual_Block, self).__init__()
-        
+
         self.conv1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False)
-        
+
     def forward(self, x): 
         identity_data = x
         output = self.relu(self.conv1(x))
@@ -21,13 +31,16 @@ class _Residual_Block(nn.Module):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        
+
+        rgb_mean = (0.4488, 0.4371, 0.4040)
+        self.sub_mean = MeanShift(rgb_mean, -1)
+
         self.conv_input = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False)
-        
+
         self.residual = self.make_layer(_Residual_Block, 32)
 
         self.conv_mid = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False)
-        
+
         self.upscale4x = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=256*4, kernel_size=3, stride=1, padding=1, bias=False),
             nn.PixelShuffle(2),
@@ -36,7 +49,9 @@ class Net(nn.Module):
         )
 
         self.conv_output = nn.Conv2d(in_channels=256, out_channels=3, kernel_size=3, stride=1, padding=1, bias=False)
-        
+
+        self.add_mean = MeanShift(rgb_mean, 1)
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -47,7 +62,7 @@ class Net(nn.Module):
                 m.weight.data.fill_(1)
                 if m.bias is not None:
                     m.bias.data.zero_()
-                
+
     def make_layer(self, block, num_of_layer):
         layers = []
         for _ in range(num_of_layer):
@@ -55,11 +70,13 @@ class Net(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.conv_input(x)
+        out = self.sub_mean(x)
+        out = self.conv_input(out)
         residual = out
         out = self.conv_mid(self.residual(out))
         out = torch.add(out,residual)
         out = self.upscale4x(out)
         out = self.conv_output(out)
+        out = self.add_mean(out)
         return out
  
--- main_edsr.py
+++ main_edsr.py
@@ -10,24 +10,23 @@ from edsr import Net
 from dataset import DatasetFromHdf5
 
 # Training settings
-parser = argparse.ArgumentParser(description='PyTorch SRResNet')
-parser.add_argument('--batchSize', type=int, default=64, help='training batch size')
-parser.add_argument('--nEpochs', type=int, default=500, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=1e-4, help='Learning Rate. Default=1e-4')
-parser.add_argument('--step', type=int, default=200, help='Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=10')
-parser.add_argument('--cuda', action='store_true', help='use cuda?')
-parser.add_argument('--resume', default='', type=str, help='path to latest checkpoint (default: none)')
-parser.add_argument('--start-epoch', default=1, type=int, help='manual epoch number (useful on restarts)')
-parser.add_argument('--clip', type=float, default=10, help='Clipping Gradients. Default=0.1')
-parser.add_argument('--threads', type=int, default=1, help='number of threads for data loader to use')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float, help='weight decay, Default: 0')
+parser = argparse.ArgumentParser(description="PyTorch EDSR")
+parser.add_argument("--batchSize", type=int, default=64, help="training batch size")
+parser.add_argument("--nEpochs", type=int, default=500, help="number of epochs to train for")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
+parser.add_argument("--step", type=int, default=200, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=10")
+parser.add_argument("--cuda", action="store_true", help="use cuda?")
+parser.add_argument("--resume", default='', type=str, help="path to latest checkpoint (default: none)")
+parser.add_argument("--start-epoch", default=1, type=int, help="manual epoch number (useful on restarts)")
+parser.add_argument("--threads", type=int, default=1, help="number of threads for data loader to use")
+parser.add_argument("--momentum", default=0.9, type=float, help="momentum")
+parser.add_argument("--weight-decay", "--wd", default=1e-4, type=float, help="weight decay, Default: 0")
 
 def main():
 
     global opt, model 
     opt = parser.parse_args()
-    print(opt)    
+    print(opt)
 
     cuda = opt.cuda
     if cuda and not torch.cuda.is_available():
@@ -41,15 +40,15 @@ def main():
 
     cudnn.benchmark = True
         
-    print('===> Loading datasets')
-    train_set = DatasetFromHdf5('rgb_edsr_x4.h5')
+    print("===> Loading datasets")
+    train_set = DatasetFromHdf5("path_to_dataset.h5")
     training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
 
-    print('===> Building model')
+    print("===> Building model")
     model = Net()
     criterion = nn.L1Loss(size_average=False)
 
-    print('===> Setting GPU')
+    print("===> Setting GPU")
     if cuda:
         model = model.cuda()
         criterion = criterion.cuda()  
@@ -59,15 +58,15 @@ def main():
         if os.path.isfile(opt.resume):
             print("=> loading checkpoint '{}'".format(opt.resume))
             checkpoint = torch.load(opt.resume)
-            opt.start_epoch = checkpoint['epoch'] + 1
-            model.load_state_dict(checkpoint['model'].state_dict())
+            opt.start_epoch = checkpoint["epoch"] + 1
+            model.load_state_dict(checkpoint["model"].state_dict())
         else:
             print("=> no checkpoint found at '{}'".format(opt.resume))
-            
-    print('===> Setting Optimizer')
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr, weight_decay=opt.weight_decay, betas = (0.9, 0.999), eps=1e-08)
 
-    print('===> Training')
+    print("===> Setting Optimizer")
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=opt.lr, weight_decay=opt.weight_decay, betas = (0.9, 0.999), eps=1e-08)
+
+    print("===> Training")
     for epoch in range(opt.start_epoch, opt.nEpochs + 1): 
         train(training_data_loader, optimizer, model, criterion, epoch)
         save_checkpoint(model, epoch)
@@ -81,7 +80,7 @@ def total_gradient(parameters):
         totalnorm += modulenorm ** 2
     totalnorm = totalnorm ** (1./2)
     return totalnorm
-    
+
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10"""
     lr = opt.lr * (0.1 ** (epoch // opt.step))
@@ -92,9 +91,9 @@ def train(training_data_loader, optimizer, model, criterion, epoch):
     lr = adjust_learning_rate(optimizer, epoch-1)
     
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr  
+        param_group["lr"] = lr  
 
-    print 'epoch =', epoch,'lr =',optimizer.param_groups[0]['lr']
+    print "epoch =", epoch,"lr =",optimizer.param_groups[0]["lr"]
     model.train()
 
     for iteration, batch in enumerate(training_data_loader, 1):
@@ -104,25 +103,29 @@ def train(training_data_loader, optimizer, model, criterion, epoch):
         if opt.cuda:
             input = input.cuda()
             target = target.cuda()
-        
+
         loss = criterion(model(input), target)
-        
+
         optimizer.zero_grad()
         
-        loss.backward()   
+        loss.backward()
 
         optimizer.step()
-        
+
         if iteration%100 == 0:
             print("===> Epoch[{}]({}/{}): Loss: {:.10f}".format(epoch, iteration, len(training_data_loader), loss.data[0]))
-            print 'total gradient', total_gradient(model.parameters())
-    
+            print "total gradient", total_gradient(model.parameters())
+
 def save_checkpoint(model, epoch):
-    model_folder = "model_adam/"
+    model_folder = "checkpoint/"
     model_out_path = model_folder + "model_epoch_{}.pth".format(epoch)
     state = {"epoch": epoch ,"model": model}
     if not os.path.exists(model_folder):
         os.makedirs(model_folder)
+
+    torch.save(state, model_out_path)
+
+    print("Checkpoint saved to {}".format(model_out_path))
 
 if __name__ == "__main__":
     main()
